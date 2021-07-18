397c761f93db8e3cac4c2f9dfec97a90f3697655
sarraff <sarraff.1@iitj.ac.in>
2021-07-18 17:18:13 +0530

    Added decision Tree:

---
+++ Distracted Driver Detection & Classification/Model_Codes/Decision Tree/Decision_tree_on_Sobel.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import cv2
+import glob
+
+# Train data on sobel processing file and save data into Sobel_train.csv
+data = pd.read_csv(r'/content/drive/MyDrive/BTP_Driver_Distraction/Sobel_train.csv'  , header = None)
+print(data)
+
+Xo = data.drop(data.columns[-1], axis=1)
+print(Xo)
+
+Yo = data[data.columns[-1]]
+print(Yo)
+
+#X_train,X_test,y_train,y_test=train_test_split(Xo , Yo , test_size = 0.15, random_state = 50)
+#print(X_train)
+#print(y_train)
+classifier = DecisionTreeClassifier()
+classifier.fit(Xo,Yo)
+#y_pred = classifier.predict(X_test)
+
+#print("Split Accuracy:",metrics.accuracy_score(y_test,y_pred))
